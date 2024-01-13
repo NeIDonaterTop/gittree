e920ceec99dbf349db524e80e7707b735e6f8686
NeIDonaterTop <90421829+NeIDonaterTop@users.noreply.github.com>
2024-01-13 18:57:24 +0300

    Создание схемы

--- tree/mongooseTasks.js
+++ tree/mongooseTasks.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-const Tree = mongoose.model('Tree', { name: String });
-const welding = new Tree({ name: 'Лист' });
+const treeSchema = new mongoose.Schema( {name: String} );
+treeSchema.methods.brew = function() {
+    console.log(this.name + ' упал');
+}
+const Tree = mongoose.model('Tree', treeSchema);
+const welding = new Tree({ name: 'Клен' });
 
 welding.save()
-  .then(() => console.log('Клен'))
+  .then(() => welding.brew())
   .catch((err) => console.log(err));
