929d560890d5886745a68aa17ece39e50eb52875
NeIDonaterTop <90421829+NeIDonaterTop@users.noreply.github.com>
2024-01-13 18:53:34 +0300

    Посев данных приложения

--- tree/createDB.js
+++ tree/createDB.js
@@ -1,28 +1,18 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb://localhost:27017/";
+var MongoClient = require('mongodb').MongoClient
 var data = require("./data.js").data
-
-const client = new MongoClient(uri);
-
+const uri = "mongodb://localhost:27017/"
+const client = new MongoClient(uri)
 async function run() {
-  try {
-    await client.connect();
-    var database = client.db("tree1");
-
-    // Drop the database
-    await database.dropDatabase();
-
-    // Create the database again
-    database = client.db("tree1");
-
-    const cats = database.collection("tree");
-    const result = await cats.insertOne({ name: "Лист Клена" });
-    
-    console.log(`${result.insertedCount} document was inserted`);
-    console.log(`Inserted document ID: ${result.insertedId}`);
-  } finally {
-    await client.close();
-  }
+        try {
+            await client.connect();
+            var database = client.db("tree1");
+            database.dropDatabase()
+            database = client.db("tree1");
+            const tree = database.collection("tree");
+            const result = await tree.insertMany(data);
+            console.log(`${result.insertedCount} documents were inserted`);
+    } finally {
+        await client.close();
+    }
 }
-
 run();
