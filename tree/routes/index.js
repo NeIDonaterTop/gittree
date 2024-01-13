81f119a836fc5fb6093b50fc0ce59bdc408c2842
NeIDonaterTop <90421829+NeIDonaterTop@users.noreply.github.com>
2024-01-13 18:19:58 +0300

    Маршрутизатор

--- tree/routes/index.js
+++ tree/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/list', function(req, res, next) { res.send("<h1>Страница Листа</h1>")});
+router.get('/koren', function(req, res, next) { res.send("<h1>Страница Корня</h1>")
 });
-
+router.get('/stvol', function(req, res, next) { res.send("<h1>Страница Ствола</h1>")
+});
 module.exports = router;
