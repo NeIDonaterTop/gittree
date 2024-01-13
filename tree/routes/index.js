3ad9eba99060c68cc3eeed5576814e7a8e4ea762
NeIDonaterTop <90421829+NeIDonaterTop@users.noreply.github.com>
2024-01-13 19:45:38 +0300

    Создание таблицы user, логики пользователя

--- tree/routes/index.js
+++ tree/routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-var checkAuth = require("./../middleware/checkAuth.js")
-const Tree = require("../models/tree").Tree;
-var User = require("./../models/user").User
+//var User = require("../models/user").User
+var db = require('../mySQLConnect.js');
+
+
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
@@ -13,35 +14,44 @@ router.get('/', async (req, res, next) => {
     next(err);
   }
 });
+
 router.get('/logreg', async function(req, res, next) {
   res.render('logreg', { title: 'Вход',error:null}); 
 });
-  router.post('/logreg', async function(req, res, next) {
-    const username = req.body.username;
-    const password = req.body.password;
-    try {
-        const user = await User.findOne({ username });
-        
-        if (user) {
-            if (user.checkPassword(password)) {
-                req.session.user = user._id;
-                res.redirect('/');
-            } else {
-                res.render('logreg', { title: 'Вход', error: 'Неверный пароль' });
-            }
-        } else {
-            const newUser = new User({ username, password });
-            await newUser.save();
-            req.session.user = newUser._id;
-            res.redirect('/');
-        }
-    } catch (err) {
-        next(err);
-    }
+
+router.post('/logreg', function(req, res, next) {
+  var username = req.body.username
+  var password = req.body.password
+  db.query (`SELECT * FROM user WHERE user.username = '${req.body.username}'`,
+  function(err,users){
+  if(err) return next(err)
+  if(users.length > 0) {
+  var user = users[0];
+  if (password == user.password){
+  req.session.user = user.id
+  res.redirect('/')
+  } else {
+  res.render('logreg', {title: 'Вход'})
+  }
+  } else {
+  db.query(`INSERT INTO user (username, password) VALUES ('${username}',
+  '${password}')`, function(err, user){
+  if(err) return next(err)
+  req.session.user = user.id
+  res.redirect('/')
+  })
+  }
+  })
   });
-  router.post('/logout', (req, res, next) => {
-    req.session.destroy();
-    res.locals.user = null;
-    res.redirect('/');
-  });
+  
+router.post('/logout', function(req, res, next) {
+  req.session.destroy()
+  res.locals.user = null
+  res.redirect('/')
+});
+  
+
+
+
+
 module.exports = router;
