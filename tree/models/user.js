8d04f40fc4ed4e4b4d7efb1d4792ced651647151
NeIDonaterTop <90421829+NeIDonaterTop@users.noreply.github.com>
2024-01-13 19:16:13 +0300

    Создали модель user

---
+++ tree/models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+
+const schemaUser = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  hashedPassword: {
+    type: String,
+    required: true
+  },
+  salt: {
+    type: String,
+    required: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+schemaUser.virtual("password").set(async function(password) {
+  this._purePassword = password;
+  this.salt = Math.random() + "";
+  this.hashedPassword = this.encryptPassword(password);
+}).get(function() {
+  return this._purePassword;
+});
+
+schemaUser.methods.encryptPassword = function(password) {
+  return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+};
+
+schemaUser.methods.checkPassword = function(password) {
+  const hashedPassword = crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+  return this.hashedPassword === hashedPassword;
+};
+
+module.exports.User = mongoose.model("User", schemaUser);
