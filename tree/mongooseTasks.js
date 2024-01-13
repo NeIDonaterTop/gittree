fac204bb0b2b586abbfc913b083b1c8d73d39a8f
NeIDonaterTop <90421829+NeIDonaterTop@users.noreply.github.com>
2024-01-13 18:58:31 +0300

    Создание модели данных приложения

--- tree/mongooseTasks.js
+++ tree/mongooseTasks.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+mongoose.connect('mongodb://127.0.0.1:27017/test1');
 
-const treeSchema = new mongoose.Schema( {name: String} );
-treeSchema.methods.brew = function() {
-    console.log(this.name + ' упал');
-}
-const Tree = mongoose.model('Tree', treeSchema);
-const welding = new Tree({ name: 'Клен' });
+const Tree = require("./models/tree").Tree;
 
-welding.save()
-  .then(() => welding.brew())
-  .catch((err) => console.log(err));
+const tree = new Tree({
+  title: "Лист Клена",
+  nick: "klenlist"
+});
+
+console.log(tree);
+
+tree.save()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+  
