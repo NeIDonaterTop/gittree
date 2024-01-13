27dc1904bc6b8a4a05e9ab226ab29970e558de1d
NeIDonaterTop <90421829+NeIDonaterTop@users.noreply.github.com>
2024-01-13 18:51:07 +0300

    Подключение базы данных

---
+++ tree/createDB.js
@@ -0,0 +1,26 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb://localhost:27017/";
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    var database = client.db("tree1");
+
+    // Drop the database
+    await database.dropDatabase();
+
+    // Create the database again
+    database = client.db("tree1");
+
+    const cats = database.collection("tree");
+    const result = await cats.insertOne({ name: "Лист Клена" });
+    
+    console.log(`${result.insertedCount} document was inserted`);
+    console.log(`Inserted document ID: ${result.insertedId}`);
+  } finally {
+    await client.close();
+  }
+}
+
+run();
