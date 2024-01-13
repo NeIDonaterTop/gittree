61d7943498481ba6a39e2ec0dd813361c94ae38b
NeIDonaterTop <90421829+NeIDonaterTop@users.noreply.github.com>
2024-01-13 19:06:46 +0300

    Заполнение меню из базы данных

--- tree/routes/tree.js
+++ tree/routes/tree.js
@@ -1,26 +1,31 @@
-var express = require('express');
-var router = express.Router();
-var Tree = require("../models/tree").Tree;
+const express = require('express');
+const router = express.Router();
+const Tree = require("../models/tree").Tree;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
     res.send('Новый маршрутизатор, для маршрутов, начинающихся с tree');
-  });
+});
 
-router.get("/:nick", async (req, res, next) => {
-  try {
-    const tree = await Tree.findOne({ nick: req.params.nick });
-    console.log(tree);
-    if (!tree) {
-      throw new Error("Нет такой части у дерева");
+/* Страница чая */
+router.get('/:nick', async (req, res, next) => {
+    try {
+        const [tree, trees] = await Promise.all([
+            Tree.findOne({ nick: req.params.nick }).exec(),
+            Tree.find({}, { _id: 0, title: 1, nick: 1 }).exec()
+        ]);
+
+        if (!tree) throw new Error("Нет такой части дерева");
+
+        res.render('tree', {
+            title: tree.title,
+            picture: tree.avatar,
+            desc: tree.desc,
+            menu:trees
+        });
+    } catch (err) {
+         next(err);
     }
-    res.render('tree', {
-      title: tree.title,
-      picture: tree.avatar,
-      desc: tree.desc
-    });
-  } catch (err) {
-    next(err);
-  }
-});       
+});
+
 module.exports = router;
