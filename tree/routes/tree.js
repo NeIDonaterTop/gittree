49bc301d2de1f67a9a0f7a23d4c3bba3232b3a58
NeIDonaterTop <90421829+NeIDonaterTop@users.noreply.github.com>
2024-01-13 19:32:06 +0300

    Вывели данные на экран

--- tree/routes/tree.js
+++ tree/routes/tree.js
@@ -1,32 +1,23 @@
-const express = require('express');
-const router = express.Router();
-const Tree = require("../models/tree").Tree;
-var checkAuth = require("./../middleware/checkAuth.js")
-
-/* GET users listing. */
-router.get('/', (req, res) => {
-    res.send('Новый маршрутизатор, для маршрутов, начинающихся с tree');
+var express = require('express');
+var router = express.Router();
+var db = require('../mySQLConnect.js');
+router.get('/', function(req, res, next) {
+res.send('<h1>Это экран для списка частей дерева</h1>');
 });
-
-/* Страница чая */
-router.get('/:nick',checkAuth, async (req, res, next) => {
-    try {
-        const [tree, trees] = await Promise.all([
-            Tree.findOne({ nick: req.params.nick }).exec(),
-            Tree.find({}, { _id: 0, title: 1, nick: 1 }).exec()
-        ]);
-
-        if (!tree) throw new Error("Нет такой части дерева");
-
-        res.render('tree', {
-            title: tree.title,
-            picture: tree.avatar,
-            desc: tree.desc,
-            menu:trees
-        });
-    } catch (err) {
-         next(err);
-    }
+router.get("/:nick", function(req, res, next) {
+db.query(`SELECT * FROM trees WHERE trees.nick = '${req.params.nick}'`, (err, trees) => {
+if(err) {
+console.log(err);
+if(err) return next(err)
+} else {
+if(trees.length == 0) return next(new Error("Нет такой части дерева"))
+var tree = trees[0];
+res.render('tree', {
+title: tree.title,
+picture: tree.avatar,
+desc: tree.about
+})
+}
+})
 });
-
 module.exports = router;
